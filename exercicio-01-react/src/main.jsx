2ef44923a02013096a2bc948f6b659ed767d4813
CarlosEduardo7700 <rm552164@fiap.com.br>
2023-09-04 15:00:23 -0300

    Configurando as rotas na main.

--- exercicio-01-react/src/main.jsx
+++ exercicio-01-react/src/main.jsx
@@ -9,9 +9,28 @@ import Produtos from './routes/Produtos/index.jsx'
 import Error from './routes/Error/index.jsx'
 import EditarProdutos from './routes/EditarProdutos/index.jsx'
 
+const router = createBrowserRouter([{
+  path: "/",
+  element: <App/>,
+  errorElement: <Error/>,
+  children: [
+    {
+      path: "/",
+      element: <Home/>
+    },
+    {
+      path:"/produtos",
+      element: <Produtos/>
+    },
+    {
+      path: "/produtos/editar/:id",
+      element: <EditarProdutos/>
+    }
+  ]
+}])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App/>
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
